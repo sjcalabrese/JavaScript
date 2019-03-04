96fa2b5f9719d7547b1b59fc12f466206ae4c8b1
Sal Calabrese <erotavlas103@gmail.com>
2019-03-04 14:07:59 -0500

    add add() script

---
+++ scripts/font.js
@@ -0,0 +1,4 @@
+function changeFont(){
+            document.getElementById('text').style.fontSize='30px';
+            document.getElementById('text').style.color='blue';
+        }
---
+++ scripts/varTest.js
@@ -0,0 +1,6 @@
+function add() {
+    var i = 10;
+    var j = 12;
+    var k = i + j;
+    document.getElementById('result').innerHTML=k;
+}
