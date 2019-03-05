f242a0fbd7136cedd1fbf3f3b493f96d814ad642
Sal Calabrese <erotavlas103@gmail.com>
2019-03-05 11:54:05 -0500

    add switch case

--- scripts/conditionStatements.js
+++ scripts/conditionStatements.js
@@ -20,8 +20,54 @@ function printConditions(){
 function checkAge(){
     var age=document.getElementById("age").value;
 
-    if( age >= 18 && age <= 60)
-        alert("you can work")
-    else
-        alert("Not eligible")
+    var message = ( age >=18 && age <= 60)?"Can work":"Can't work"
+    alert(message)
+}
+
+function whatPartOfTheWeekIsIt(){
+    var week = new Date().getDay()
+
+    switch(week){
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            alert("Weekday")
+            break;
+        case 5:
+            alert("Friday")
+            break;
+        case 6:
+        case 0:
+            alert("Weekend")
+            break;
+    }
+}
+
+function whatDayIsIt(){
+    var day = new Date().getDay()
+
+    switch(day){
+        case 1:
+            alert("Monday")
+            break;
+        case 2:
+            alert("Tuesday")
+            break;
+        case 3:
+            alert("Wednesday")
+            break;
+        case 4:
+            alert("Thursday")
+            break;
+        case 5:
+            alert("Friday")
+            break;
+        case 0:
+            alert("Sunday")
+            break;
+        case 6:
+            alert("Saturday")
+            break;
+}
 }
