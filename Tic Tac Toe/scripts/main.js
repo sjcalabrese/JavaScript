3242b2063ad35efe171c8fd86a96e32c020ce471
Sal Calabrese <erotavlas103@gmail.com>
2019-03-14 09:02:02 -0400

    Add condition to stop input after a player wins

--- Tic Tac Toe/scripts/main.js
+++ Tic Tac Toe/scripts/main.js
@@ -10,14 +10,15 @@ function init() {
    /*main function for the tic tac toe game
    iterates through the array of boxes and then waits for a click inside a box
    */
-    for (let i = 0; i < boxes.length; i++) {
+    for (let i = 0; i < boxes.length; i++) {        
         boxes[i].onclick = function () {
-            if (this.innerHTML !== "X" && this.innerHTML !== "O") { //verifies the box is empty
+
+            if (this.innerHTML !== "X" && this.innerHTML !== "O" && getWinner() !== true) { //verifies the box is empty
                 if (turn === 0) { //runs if it is player ones turn
                     console.log(turn);
                     this.innerHTML = "X"; //Marks an "X" in the box for player 1
                     playerTurn.innerHTML = "Turn = player 2"
-                    getWinner(); //test to see if the player one
+                    getWinner(); //test to see if the player one                   
                     turn = 1; //switch to player 2
                 } else { //run if it is player 2's turn
                     console.log(turn);
@@ -27,8 +28,9 @@ function init() {
                     turn = 0; //switch back to player 1
                 } 
             } 
-        } 
+        }
     } 
+     
 }
 
 function getWinner() { //assign each div to a box variable
@@ -43,38 +45,57 @@ function getWinner() { //assign each div to a box variable
     var box9 = document.getElementById("div9")
 
     //test all posibilities
-    if (box1.innerHTML !== "" && box1.innerHTML === box2.innerHTML && box1.innerHTML === box3.innerHTML)
+    if (box1.innerHTML !== "" && box1.innerHTML === box2.innerHTML && box1.innerHTML === box3.innerHTML){
         selectWinnerBoxes(box1, box2, box3);
+        return true;
+    }
     //1st row horizontal win
-    if (box4.innerHTML !== "" && box4.innerHTML === box5.innerHTML && box4.innerHTML === box6.innerHTML)
+    if (box4.innerHTML !== "" && box4.innerHTML === box5.innerHTML && box4.innerHTML === box6.innerHTML){
         selectWinnerBoxes(box4, box5, box6);
+        return true;
+    }
     //2nd row horizontal win
-    if (box7.innerHTML !== "" && box7.innerHTML === box8.innerHTML && box7.innerHTML === box9.innerHTML)
+    if (box7.innerHTML !== "" && box7.innerHTML === box8.innerHTML && box7.innerHTML === box9.innerHTML){
         selectWinnerBoxes(box7, box8, box9)
+        return true;
+    }
     //3rd row horizontal win
-    if (box1.innerHTML !== "" && box1.innerHTML === box4.innerHTML && box1.innerHTML === box7.innerHTML)
+    if (box1.innerHTML !== "" && box1.innerHTML === box4.innerHTML && box1.innerHTML === box7.innerHTML){
         selectWinnerBoxes(box1, box4, box7);
+        return true;
+    }
     //1 column verticle win
-    if (box2.innerHTML !== "" && box2.innerHTML === box5.innerHTML && box2.innerHTML === box8.innerHTML)
+    if (box2.innerHTML !== "" && box2.innerHTML === box5.innerHTML && box2.innerHTML === box8.innerHTML){
         selectWinnerBoxes(box2, box5, box8)
+        return true;
+    }
     //2nd column verticle winner
-    if (box3.innerHTML !== "" && box3.innerHTML === box6.innerHTML && box3.innerHTML === box9.innerHTML)
+    if (box3.innerHTML !== "" && box3.innerHTML === box6.innerHTML && box3.innerHTML === box9.innerHTML){
         selectWinnerBoxes(box3, box6, box9)
+        return true;
+    }
     //3rd column verticle
-    if (box1.innerHTML !== "" && box1.innerHTML === box5.innerHTML && box1.innerHTML === box9.innerHTML)
+    if (box1.innerHTML !== "" && box1.innerHTML === box5.innerHTML && box1.innerHTML === box9.innerHTML){
         selectWinnerBoxes(box1, box5, box9)
+        return true;
+    }
     //top left to bottom right diaginal
-    if (box3.innerHTML !== "" && box3.innerHTML === box5.innerHTML && box3.innerHTML === box7.innerHTML)
+    if (box3.innerHTML !== "" && box3.innerHTML === box5.innerHTML && box3.innerHTML === box7.innerHTML){
         selectWinnerBoxes(box3, box5, box7)
+        return true;
+    }
     //top right to bottom left diaginal
     if(box1.innerHTML !=="" && box2.innerHTML !== "" && box3.innerHTML !== "" && box4.innerHTML !== ""
     && box5.innerHTML !== "" && box6.innerHTML !== "" && box7.innerHTML !== "" && box8.innerHTML !== ""
-    && box9.innerHTML !== "")
+    && box9.innerHTML !== ""){
         draw();
+        return true;
+    }
+       
 }
 
 function draw(){
-    playerTurn.innerHTML = "DRAW";
+    playerTurn.innerHTML = "DRAW";  
     playerTurn.style.fontSize = "25px";
 }
 
@@ -85,12 +106,8 @@ function selectWinnerBoxes(b1, b2, b3) {
     b3.className = "win";
     playerTurn.innerHTML = b1.innerHTML + " Won, Congrats";
     playerTurn.style.fontSize = "25px";
+    
 }
-
-
-
-
-
 
 function resetgame() {
     location.reload();
